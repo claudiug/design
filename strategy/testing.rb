a292219fcbb06a210ae0dada72656d6dd28f984a
claudiug <claudiu.garba@gmail.com>
2013-12-09 22:04:51 +0100

    add report class

---
+++ strategy/testing.rb
@@ -0,0 +1,34 @@
+class Formater
+
+  def out_report(title, text)
+    raise 'Abstract method'
+  end
+end
+
+class HtmlFormater < Formater
+
+  def out_report(title, text)
+    puts 'here we have some html'
+  end
+end
+
+class PlainFormater < Formater
+
+  def out_report(title, text)
+    puts 'plain text here'
+  end
+end
+
+class Report
+  attr_reader :title, :text #just getter
+  attr_accessor :formater #getter and setter
+
+  def initialize(formater)
+    @title = title
+    @text = text
+    @formater = formater
+  end
+  def out_report
+    @formater.out_report(@title, @text)
+  end
+end
