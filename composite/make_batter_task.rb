6438db6582264aa510d04d13194966387853ef2b
claudiug <claudiu.garba@gmail.com>
2013-12-18 22:51:48 +0100

    composite

---
+++ composite/make_batter_task.rb
@@ -0,0 +1,23 @@
+class MakeBatterTask < Task
+
+  def initialize
+    super('make batter baby')
+    @sub_tasks = []
+    add_sub_task(AddDryTasks.new)
+    add_sub_task(MixTask.new)
+  end
+
+  def add_sub_task(task)
+    @sub_tasks << task
+  end
+
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+  end
+
+  def get_time_required
+    time = 0.0
+    @sub_tasks.each{|task| time +=task.get_time_required}
+    time
+  end
+end
